257f6ec194a00809543d34957113063157e0944e
Jerome Martin <jxm@risingtidesystems.com>
2011-06-11 01:08:56 +0200

    Refactored command parameter check.
    * Error messages are now more descriptive regrading wrong parameters errors.
    * Used to catch TypeError exception after the fact, without distinguishing
      between actually wrong user-supplied parameters from improper function calls
      from within our code. Also, was based on greping substrigns in the TypeError
      message, which is fragile and hackish.
    * Does not catch TypeError anymore, instead added an assert_params() method
      that check the user parameters agains the command method before calling it.

--- configshell/node.py
+++ configshell/node.py
@@ -1355,9 +1355,9 @@ class ConfigNode(object):
         they are interpreted by ConfigShell.
         @rtype: str or ConfigNode or None
         '''
-        self.log.debug("Executing command %s " % command \
-                       + "with pparams %s " % str(pparams) \
-                       + "and kparams %s." % str(kparams))
+        self.log.debug("Executing command %s " % command
+                       + "with pparams %s " % pparams
+                       + "and kparams %s." % kparams)
 
         if command in self.list_commands():
             method = self.get_command_method(command)
@@ -1366,23 +1366,97 @@ class ConfigNode(object):
             return
 
         try:
+            self.assert_params(method, pparams, kparams)
             result = method(*pparams, **kparams)
-        except TypeError, msg:
-            # TODO Find a cleaner way to do this
-            msg = str(msg)
-            if "takes at most" in msg \
-               or "takes exactly" in msg \
-               or "takes at least" in msg \
-               or "unexpected keyword" in msg:
-                self.log.error("Wrong parameters for %s, see 'help %s'."\
-                              % (command, command))
-            else:
-                raise
         except ExecutionError, msg:
             self.log.error(msg)
         else:
             return result
 
+    def assert_params(self, method, pparams, kparams):
+        '''
+        Checks that positional and keywork parameters match a method
+        definition, or raise an ExecutionError.
+        @param method: The method to check call signature against.
+        @type method: method
+        @param pparams: The positional parameters.
+        @type pparams: list
+        @param kparams: The keywork parameters.
+        @type kparams: dict
+        @raise ExecutionError: When the check fails.
+        '''
+        spec = inspect.getargspec(method)
+        args = spec.args[1:]
+        pp = spec.varargs
+        kw = spec.keywords
+
+        if spec.defaults is None:
+            nb_opt_params = 0
+        else:
+            nb_opt_params = len(spec.defaults)            
+        nb_max_params = len(args)
+        nb_min_params = nb_max_params - nb_opt_params
+        
+        req_params = args[:nb_min_params]
+        opt_params = args[nb_min_params:]
+        
+        unexpected_keywords = sorted(set(kparams) - set(args))
+        missing_params = sorted(set(args[len(pparams):]) \
+                                - set(opt_params) \
+                                - set(kparams.keys()))
+                
+        nb_params = len(pparams) + len(kparams)
+        nb_standard_params = len(pparams) \
+                + len([param for param in kparams if param in args])
+        nb_extended_params = nb_params - nb_standard_params
+        
+        self.log.debug("Min params: %d" % nb_min_params)
+        self.log.debug("Max params: %d" % nb_max_params)
+        self.log.debug("Required params: %s" % ", ".join(req_params))
+        self.log.debug("Optional params: %s" % ", ".join(opt_params))
+        self.log.debug("Got %s standard params." % nb_standard_params)
+        self.log.debug("Got %s extended params." %  nb_extended_params)
+        self.log.debug("Variable positional params: %s" % pp)
+        self.log.debug("Variable keyword params: %s" % kw)
+                
+        if len(missing_params) == 1:
+            raise ExecutionError(
+                "Missing required parameter %s"
+                % missing_params[0])
+        elif missing_params:
+            raise ExecutionError(
+                "Missing required parameters %s"
+                % ", ".join("'%s'" % missing for missing in missing_params))
+        
+        if spec.keywords is None:
+            if len(unexpected_keywords) == 1:
+                raise ExecutionError(
+                    "Unexpected keyword parameter '%s'."
+                    % unexpected_keywords[0])
+            elif unexpected_keywords:
+                raise ExecutionError(
+                    "Unexpected keyword parameters %s."
+                    % ", ".join("'%s'" % kw for kw in unexpected_keywords))
+            
+        all_params = args[:len(pparams)] + kparams.keys()
+        for param in all_params:
+            if all_params.count(param) > 1:
+                raise ExecutionError(
+                    "Duplicate parameter %s."
+                    % param)
+            
+        if nb_opt_params == 0 \
+           and nb_standard_params != nb_min_params \
+           and pp is None:
+            raise ExecutionError(
+                "Got %d positionnal parameters, expected exactly %d."
+                % (nb_standard_params, nb_min_params))
+    
+        if nb_standard_params > nb_max_params and pp is None:
+            raise ExecutionError(
+                "Got %d positionnal parameters, expected at most %d."
+                % (nb_standard_params, nb_max_params))
+        
     def list_commands(self):
         '''
         @return: The list of user commands available for this node.
