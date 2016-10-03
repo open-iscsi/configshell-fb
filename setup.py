8feb09b67f6e87ee4920216ab8e647f8758d689e
Andy Grover <agrover@redhat.com>
2016-10-03 11:09:20 -0700

    version 1.1.fb21
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name = 'configshell-fb',
-    version = '1.1.20',
+    version = '1.1.21',
     description = 'A framework to implement simple but nice CLIs.',
     license = 'Apache 2.0',
     maintainer = 'Andy Grover',
