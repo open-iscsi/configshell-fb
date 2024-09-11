790932eba614b83ee4279d04f6c35d354d0d5ea1
Martin Hoyer <mhoyer@redhat.com>
2024-09-11 13:39:39 +0200

    Add pypi publish and pre-commit workflows

--- configshell_fb.py
+++ configshell_fb.py
@@ -1,6 +1,6 @@
 # Providing backwards compatibility for modules importing 'configshell_fb'
 
-from configshell import Console, Log, ConfigNode, ExecutionError, Prefs, ConfigShell
+from configshell import ConfigNode, ConfigShell, Console, ExecutionError, Log, Prefs
 
 __all__ = [
     'Console',
